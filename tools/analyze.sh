2d97285c7b27c03e048f5f052a6ac5ac25df0532
Abdullah Asim Ersin <aae@example.com>
2026-09-10 23:09:39 +0300

    chore: Clean up legacy uBlock Origin files and organize repo

--- tools/analyze.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# AAEBlocker Brain Matrix Analyzer Shell Wrapper
-echo "Skynet Analyzer baslatiliyor..."
-python3 /home/ersin/Masaüstü/PROJELERIM/uBlock-master/tools/analyze_brain.py "$1"
